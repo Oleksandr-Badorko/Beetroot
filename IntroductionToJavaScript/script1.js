35213afae2bbb482f7b8cabf93b351e3c581cfca
Badorko Oleksandr <sachabv1999@ukr.net>
2023-04-07 23:13:10 +0300

    Homework-29

---
+++ IntroductionToJavaScript/script1.js
@@ -0,0 +1,42 @@
+// const last_name = "Prokopan";
+// let age = "30";
+// const first_name = "Denis";
+// const user_name = "Copycat";
+
+//Неправильні імена:
+
+//let name_last (зворотний порядок слів);
+//firstname (відсутнє підкреслення між словами);
+//firstName_lastName (використання camelCase замість snake_case);
+//Age (перша літера з великої літери, що не рекомендується для змінних);
+//Username (не правильне використання camelCase);
+
+
+// __________________________________________________________
+
+// let name = prompt("Please enter your username:");
+// console.log('Hello, ' + name + '!');
+
+// let yearOfBirth = prompt("Please enter your year of birth:");
+// const currentYear = "2023";
+// let age = currentYear - yearOfBirth;
+// console.log("Your age is: " + age);
+
+// let side = prompt("Please enter the length of the side of the square:");
+// let perimeter = 4 * side;
+// console.log("The perimeter of the square is: " + perimeter);
+
+// let radius = prompt("Enter the radius of the circle: ");
+// let area = Math.PI * radius * radius;
+// console.log("The area of the circle with radius " + radius + " is " + area);
+
+// let distance = prompt("Enter the distance between the two cities in kilometers: ");
+// let hours = prompt("Enter how many hours you want to take to get there: ");
+// let speed = distance / hours;
+// console.log("To cover a distance of " + distance + " kilometers in " + hours + 
+// " hours, you need to move at a speed of " + speed + " kilometers per hour.");
+
+// const exchangeRate = 0.85;
+// let dollars = prompt("Enter the amount in dollars to convert: ");
+// let euros = dollars * exchangeRate;
+// console.log("$" + dollars + " is equivalent to " + euros + " euros.");
