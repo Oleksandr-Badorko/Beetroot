f20b3b56aef9a77c85181eb5aa7edb3b3e7d2ae6
Badorko Oleksandr <sachabv1999@ukr.net>
2023-04-12 21:06:28 +0300

    Homework-30

---
+++ TypesAndOperators/script.js
@@ -0,0 +1,39 @@
+// let num1 = 0.1;
+// let num2 = 0.2;
+// let result = (num1 + num2).toFixed(1);
+
+// console.log(result);
+
+
+// let string = "1";
+// let number = 2;
+// let result = parseInt(string) + number;
+
+// console.log(result);
+
+
+// let flashDriveSizeInGB = prompt("Enter the size of the flash drive in GB:");
+
+// let flashDriveSizeInMB = flashDriveSizeInGB * 1024;
+
+// let numFiles = Math.floor(flashDriveSizeInMB / 820);
+
+// console.log(`The flash drive can hold ${numFiles} files of 820 MB each.`);
+
+
+// let walletAmount = prompt("Enter the amount of money in your wallet:");
+// let chocolateBarPrice = prompt("Enter the price of one chocolate bar:");
+
+// let numChocolates = Math.floor(walletAmount / chocolateBarPrice);
+// let change = walletAmount % chocolateBarPrice;
+
+// console.log(`You can buy ${numChocolates} chocolate bars and have ${change} left in your wallet.`);
+
+
+// let number = prompt("Enter a three-digit number:");
+
+// let digit1 = number % 10;
+// let digit2 = Math.floor(number / 10) % 10;
+// let digit3 = Math.floor(number / 100);
+
+// console.log(`The number ${number} backwards is ${digit1}${digit2}${digit3}.`);
